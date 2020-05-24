399e00399ffe92e53b75834e4b0a48e08c314bd0
sosisusy <qwexodn@gmail.com>
2020-05-24 23:12:32 +0900

    save

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
-import "./Main.scss"
+import "./Main.scss";
 
-import ReactionContainer from "components/reaction/ReactionContainer"
-import ReactionTableContainer from "components/reaction-table/ReactionTableContainer"
-import { AreaChart, CartesianGrid, XAxis, YAxis, Tooltip, Area, Label } from "recharts";
+import ReactionContainer from "components/reaction/ReactionContainer";
+import ReactionTableContainer from "components/reaction-table/ReactionTableContainer";
+import { AreaChart, CartesianGrid, XAxis, YAxis, Tooltip, Area, ResponsiveContainer } from "recharts";
 
 class Main extends Component {
-
     constructor(props) {
-        super(props)
+        super(props);
+
         this.state = {
             data: [],
-        }
+        };
     }
 
     // 데이터 저장
     dataSaveHandle(time) {
-        console.log(time)
-        const { data } = this.state
+        console.log(time);
+        const { data } = this.state;
 
-        let newData = [...data, time]
+        let newData = [...data, time];
 
         this.setState({
             data: newData,
-        })
+        });
     }
 
     chartData(data) {
@@ -31,32 +31,34 @@ class Main extends Component {
             return {
                 name: index + 1,
                 value: time,
-            }
-        })
+            };
+        });
     }
 
     render() {
-        const { data } = this.state
-        let chartData = this.chartData(data)
+        const { data } = this.state;
+        let chartData = this.chartData(data);
 
         return (
             <section className="main__section">
-                <h1 className='title'>반응속도 테스트</h1>
-                <ReactionContainer onSave={(time) => this.dataSaveHandle(time)} />
+                <h1 className="title">반응속도 테스트</h1>
+                <ReactionContainer onSave={time => this.dataSaveHandle(time)} />
                 {/* <ReactionTableContainer data={data} /> */}
-                <AreaChart width={800} height={300} data={chartData} style={{ marginTop: "10px" }} margin={{ left: 0, right: 10, top: 20 }}>
-                    <defs>
-                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="#71d2f8" stopOpacity={0.6} />
-                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-                        </linearGradient>
-                    </defs>
-                    <Area type="monotone" dataKey="value" stroke="#71d2f8" fillOpacity={1} fill="url(#colorUv)" />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="1 2" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                </AreaChart>
+                <ResponsiveContainer width="100%" height={300}>
+                    <AreaChart data={chartData} style={{ marginTop: "10px" }} margin={{ left: 0, right: 10, top: 20 }}>
+                        <defs>
+                            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#71d2f8" stopOpacity={0.6} />
+                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
+                        <Area type="monotone" dataKey="value" stroke="#71d2f8" fillOpacity={1} fill="url(#colorUv)" />
+                        <CartesianGrid stroke="#ccc" strokeDasharray="1 2" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                    </AreaChart>
+                </ResponsiveContainer>
             </section>
         );
     }
